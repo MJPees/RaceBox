d7740de4704278fc1e73b4121fb14f107d11bdac
René Kersten <rene.kersten@gmail.com>
2025-06-14 15:46:24 +0200

    feat: add releases.json

--- RaceBox-GhostCar/config.h
+++ RaceBox-GhostCar/config.h
@@ -1,7 +1,7 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define VERSION "1.1.0"
+#define VERSION "1.1.0" // dont forget to update the releases.json
 
 #define WIFI_DEFAULT_HOSTNAME "racebox-ghostcar"
 
