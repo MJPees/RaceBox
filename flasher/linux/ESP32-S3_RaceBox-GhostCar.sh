ef725fc97d32076b18105fb6351f9dff86cbbcd7
MJPees <marc.pees@l-n-s.de>
2025-06-27 18:43:05 +0200

    removed old flash-files, added files for github-workflow

--- flasher/linux/ESP32-S3_RaceBox-GhostCar.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./esptool --chip esp32s3 --port /dev/ttyACM0 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode keep --flash_freq keep --flash_size keep 0x0 ../bin/ESP32-S3/RaceBox-GhostCar.ino.bootloader.bin 0x8000 ../bin/ESP32-S3/RaceBox-GhostCar.ino.partitions.bin 0xe000 ../bin/ESP32-S3/boot_app0.bin 0x10000 ../bin/ESP32-S3/RaceBox-GhostCar.ino.bin
