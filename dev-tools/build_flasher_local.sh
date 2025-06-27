d28299882447cfe698b8be56eb05a8df32c6da9b
MJPees <marc.pees@l-n-s.de>
2025-06-28 00:07:19 +0200

    fixes

--- dev-tools/build_flasher_local.sh
+++ dev-tools/build_flasher_local.sh
@@ -4,6 +4,10 @@
 cd ..
 sudo ../../bin/act -W ./.github/workflows/build-RaceBox-ESP32-DEV.yml --rm --pull --artifact-server-path ./local_artifacts
 sudo ../../bin/act -W ./.github/workflows/build-RFID-Label-Writer-ESP32-DEV.yml --rm --pull --artifact-server-path ./local_artifacts
+sudo ../../bin/act -W ./.github/workflows/build-RaceBox-GhostCar-ESP32-C3-Poti.yml --rm --pull --artifact-server-path ./local_artifacts
+sudo ../../bin/act -W ./.github/workflows/build-RaceBox-GhostCar-ESP32-C3-NoPoti.yml --rm --pull --artifact-server-path ./local_artifacts
+sudo ../../bin/act -W ./.github/workflows/build-RaceBox-GhostCar-ESP32-S3-Poti.yml --rm --pull --artifact-server-path ./local_artifacts
+sudo ../../bin/act -W ./.github/workflows/build-RaceBox-GhostCar-ESP32-S3-NoPoti.yml --rm --pull --artifact-server-path ./local_artifacts
 sudo chmod -R 777 ./local_artifacts
 ls -lR ./local_artifacts
 cd dev-tools
