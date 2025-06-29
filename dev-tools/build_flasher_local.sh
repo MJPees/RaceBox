22e8a34fdaa3ca26240bb414a18151a67243cc26
MJPees <marc.pees@l-n-s.de>
2025-06-29 02:37:38 +0200

    added flash instructions for windows, added startlinglightsdisplay flasher

--- dev-tools/build_flasher_local.sh
+++ dev-tools/build_flasher_local.sh
@@ -8,6 +8,7 @@ sudo ../../bin/act -W ./.github/workflows/build-RaceBox-GhostCar-ESP32-C3-Poti.y
 sudo ../../bin/act -W ./.github/workflows/build-RaceBox-GhostCar-ESP32-C3-NoPoti.yml --rm --pull --artifact-server-path ./local_artifacts
 sudo ../../bin/act -W ./.github/workflows/build-RaceBox-GhostCar-ESP32-S3-Poti.yml --rm --pull --artifact-server-path ./local_artifacts
 sudo ../../bin/act -W ./.github/workflows/build-RaceBox-GhostCar-ESP32-S3-NoPoti.yml --rm --pull --artifact-server-path ./local_artifacts
+sudo ../../bin/act -W ./.github/workflows/build-RaceBox-StartingLightsDisplay-ESP32-C6.yml --rm --pull --artifact-server-path ./local_artifacts
 sudo chmod -R 777 ./local_artifacts
 ls -lR ./local_artifacts
 cd dev-tools
